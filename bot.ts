4ce31ddeee93449ceb185c0d941be54c84a731eb
abias <abias1122@gmail.com>
2023-04-18 19:04:56 -0400

    Change AI model of AI bot

--- bot.ts
+++ bot.ts
@@ -26,13 +26,13 @@ const removeMention = (text: string) =>
 
 const generateArt = (prompt: string) =>
   replicate.run(
-    'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf',
+    'ai-forever/kandinsky-2:601eea49d49003e6ea75a11527209c4f510a93e2112c969d548fbb45b9c4f19f',
     {
       input: {
         prompt,
-        num_outputs: 4,
-        negative_prompt:
-          'penis, vagina, pornography, horny, rape, nudity, ejaculation, testicles',
+        width: 768,
+        height: 768,
+        batch_size: 3,
       },
     }
   );
