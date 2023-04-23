a6b97a65cee272b916d262150dee016bf1b7e665
abias <abias1122@gmail.com>
2023-04-23 02:07:23 -0400

    Make bot make automated posts

--- bot.ts
+++ bot.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env ts-node
 
-import LemmyBot from 'lemmy-bot';
+import LemmyBot, { PostFeatureType, SortType } from 'lemmy-bot';
 import Replicate from 'replicate';
 import { config } from 'dotenv';
 
@@ -32,7 +32,7 @@ const generateArt = (prompt: string) =>
         prompt,
         width: 768,
         height: 768,
-        batch_size: 3,
+        batch_size: 4,
       },
     }
   );
@@ -45,7 +45,13 @@ const generateReply = (prompt: string, res: string[]) =>
     .join('\n')}\n:::`;
 
 const mentionIsInRightCommunity = (actorId: string) =>
-  new RegExp(`https?:\\/\\/${INSTANCE}\\/c\\/aiart`).test(actorId);
+  new RegExp(`https?:\\/\\/${INSTANCE.replace(/:.*/, '')}\\/c\\/aiart`).test(
+    actorId
+  );
+
+const threadRegex = /[A-Za-z\s]*(\d+)/;
+
+let currentThread: number | undefined = undefined;
 
 const bot = new LemmyBot({
   instance: INSTANCE,
@@ -67,10 +73,11 @@ const bot = new LemmyBot({
       mentionView: {
         comment: { id, post_id, content },
         community: { actor_id },
+        post: { id: postId },
       },
       botActions: { createComment },
     }) {
-      if (mentionIsInRightCommunity(actor_id)) {
+      if (mentionIsInRightCommunity(actor_id) && postId === currentThread) {
         const prompt = removeMention(content).trim().replace(/\n/g, '');
         try {
           const res = await generateArt(prompt);
@@ -99,11 +106,60 @@ const bot = new LemmyBot({
         }
       } else {
         createComment({
-          content: `My apologies comrade, but I only create art in the [AI art community](https://${INSTANCE}/c/aiart). Mention me in a comment there and give me a prompt and I'll make art for you.`,
+          content: `My apologies comrade, but I only create art in the active art thread in the [AI art community](https://${INSTANCE}/c/aiart).\n\n[Here is the current art thread.](https://${INSTANCE}/post/${currentThread}) Mention me in a comment there and give me a prompt and I'll make art for you.`,
           postId: post_id,
           parentId: id,
         });
       }
+    },
+    post: {
+      sort: SortType.Active,
+      minutesUntilReprocess: 1,
+      async handle({
+        postView: {
+          creator: { actor_id },
+          post: { locked, id, name, featured_community },
+          counts: { comments },
+        },
+        botActions: { featurePost, lockPost, createPost, getCommunityId },
+      }) {
+        if (
+          actor_id.includes(
+            `${INSTANCE.replace(/:.*/, '')}/u/${USERNAME_OR_EMAIL}`
+          ) &&
+          !locked
+        ) {
+          if (!currentThread) {
+            currentThread = id;
+          }
+
+          if (comments >= 5) {
+            lockPost({ locked: true, postId: id });
+            featurePost({
+              postId: id,
+              featured: false,
+              featureType: PostFeatureType.Community,
+            });
+
+            const communityId = await getCommunityId('aiart');
+
+            if (communityId) {
+              createPost({
+                communityId,
+                name: `Art Thread ${
+                  parseInt(name.match(threadRegex)![1], 10) + 1
+                }`,
+              });
+            }
+          } else if (!featured_community) {
+            featurePost({
+              postId: id,
+              featured: true,
+              featureType: PostFeatureType.Community,
+            });
+          }
+        }
+      },
     },
   },
 });
